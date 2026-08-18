fa46f638726e8bab3bf8700f7a4e30f0f823daf3
yubeen0412 <lyubeen07@gmail.com>
2026-08-19 05:46:46 +0900

    practice 3

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define SIZE 5
 
 int main(void)
 {
-    int i, average;
-	int sum;
+    int i;
     int grade[SIZE];
-    
-    sum=0;
+    int score[SIZE];
 
-    for (i=0; i < SIZE; i++)
-    {
-    	printf("enter student's grades : ");
-	    scanf("%d", &grade[i]);
-		
-		sum += grade[i];
-	}
-    
-    average = sum/SIZE;
-    
-    printf("average : %d\n", average);
-    
+    for (i = 0; i < SIZE; i++)
+        grade[i] = rand() % 100;
+
+    for (i = 0; i < SIZE; i++)
+        score[i] = grade[i];
+
+    for (i = 0; i < SIZE; i++)
+        printf("score[%d] = %d\n", i, score[i]);
+
     return 0;
 }
