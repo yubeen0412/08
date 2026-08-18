9664e5a4fc1c6e95315d5d3896b96cbbdd487dbb
yubeen0412 <lyubeen07@gmail.com>
2026-08-19 05:55:43 +0900

    practice 3-1

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define SIZE 5
 
 int main(void)
 {
     int i;
-    int grade[SIZE];
-    int score[SIZE];
+    int a[SIZE] = {1, 2, 3, 4, 5};
+    int b[SIZE] = {1, 2, 3, 4, 5};
+	int flag_same = 1;
 
-    for (i = 0; i < SIZE; i++)
-        grade[i] = rand() % 100;
+	for (i = 0; i < SIZE; i++)
+	{
+    if (a[i] != b[i])
+    {
+        flag_same = 0;
+    }
+	}
 
-    for (i = 0; i < SIZE; i++)
-        score[i] = grade[i];
-
-    for (i = 0; i < SIZE; i++)
-        printf("score[%d] = %d\n", i, score[i]);
-
-    return 0;
+if (flag_same == 1)
+    printf("Arrays have same values.\n");
+else
+    printf("Arrays have different values.\n");
 }
